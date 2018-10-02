3b07d7f35842a96e9a1a0491ee48068d78f5c221
falahulef <43511136+falahulef@users.noreply.github.com>
2018-10-02 16:56:48 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "localhost";
+$username ="root";
+$password = "";
+$db = "mahasiswa";
+$con = new mysqli($servername,$username,$password,$db);
+
+if($con==false){
+	die("Connection Failed: ". $con->connect_eror);
+}
+
+ ?>
