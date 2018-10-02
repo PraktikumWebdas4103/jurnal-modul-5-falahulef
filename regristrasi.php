5845e85c582cdf00130a10ba435a93f3eb93df3e
falahulef <43511136+falahulef@users.noreply.github.com>
2018-10-02 16:55:53 +0700

    Update regristrasi.php

--- regristrasi.php
+++ regristrasi.php
@@ -1,11 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Form Registrasi</title>
-</head>
-<body>
+
+<form action="prosesregris.php" method="POST">
+<table>
+	<tr>
+	<td>Nim</td> <td>:</td> <td> <input type="number" name="nim" maxlength="10" required="error" >
+	</tr> 
+	<tr>
+	<td>Nama</td> <td>:</td> <td><input type="text" name="nama" maxlength="25" required="error"> </td></td>
+	</tr>
+	<tr>
+	<td>E-Mail</td> <td>:</td> <td><input type="email" name="email" required="error" > </td></td>
+	</tr>
+	<tr>
+	<td></td><td></td><td align="right"><input type="submit" name="submit" value="OK BRUH"></td>
+	</tr>
+</table>
+</form>
+
+<?php
 
 
-
-</body>
-</html>
+ ?>
