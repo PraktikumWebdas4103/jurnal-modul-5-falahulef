1e4152db26c059abbb11c2a9327174005ea3fc44
falahulef <43511136+falahulef@users.noreply.github.com>
2018-10-02 16:51:48 +0700

    Update komentar.php

--- komentar.php
+++ komentar.php
@@ -1,3 +1,18 @@
-<?php
+<form action="#" method="POST">
+<table>
+<tr>
+<td>Komentar</td> <td>:</td> <td><textarea style="height: 120; width: 150;" name="komen"></textarea></td>
+</tr>
+<tr>
+<td></td><td></td><td><input type="submit" name="submit" value="Hitung Panjang"></td>
+</tr>
+</table>
+</form>
 
+<?php  
+if (isset($_POST['submit'])) {
+	$kata = $_POST['komen'];
+	$hitung = str_word_count($kata);
+	echo "Jumlah Kata String di atas adalah $hitung";
+}
 ?>
