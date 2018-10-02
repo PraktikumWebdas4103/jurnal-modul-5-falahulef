a8ad73747b18f9d0d93cd428b885b3cac8242919
falahulef <43511136+falahulef@users.noreply.github.com>
2018-10-02 16:54:57 +0700

    Update prosesregris.php

--- prosesregris.php
+++ prosesregris.php
@@ -1,4 +1,17 @@
 <?php
+include "koneksi.php";
 
+if(isset($_POST['submit'])) {
+	$nim 	= $_POST['nim'];
+	$nama 	= $_POST['nama'];
+	$email 	= $_POST['email'];
 
-?>
+	$sql ="INSERT INTO profile (nim,nama,email) values ('$nim','$nama','$email')";
+	if (mysqli_query($con,$sql)){
+		echo "Berhasil Input Data";
+	}else{
+		echo "Gagal Input Data";
+	}
+	}
+
+ ?>
